a624105311aa464c9ecee3d190cebf6d01d54db8
Liceth35 <liceth2006valderrama@gmail.com>
2024-07-20 13:10:48 -0500

    prueba

--- vista_certificados.php
+++ vista_certificados.php
@@ -33,7 +33,6 @@ $db->close(); // Cerrar la conexión después de obtener los datos
     <link rel="stylesheet" href="./css/vista_certificados.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="shortcut icon" href="images/New_Logo_EyC2024_vertical-removebg-preview.png">
-    
 </head>
 <body>
    <form action="controlador/certificados_excel.php" method="post" class="btn-descargar">
