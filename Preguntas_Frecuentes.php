7188129840f26eac3502e86a49856f5f37bb53f3
liceth valderrama <liceth2006valderrama@gmail.com>
2024-10-08 07:21:53 -0500

    ultimos cambios

--- Preguntas_Frecuentes.php
+++ Preguntas_Frecuentes.php
@@ -223,10 +223,62 @@
         </div>
 
     </div>
+    <!-- Footer -->
+    <section id="footer">
+        <div class="container">
+            <div class="row text-center text-xs-center text-sm-left text-md-left">
+                <div class="col-xs-12 col-sm-4 col-md-4">
+                    <h5>Accesos Rápidos</h5>
+                    <ul class="list-unstyled quick-links">
+                        <li><a href="./index.php"><i class="fa fa-angle-double-right"></i>Inicio</a></li>
+                        <li><a href="./nosotros.php"><i class="fa fa-angle-double-right"></i>Nosotros</a></li>
+                        <li><a href="./servicios.php"><i class="fa fa-angle-double-right"></i>Servicios</a></li>
+                        <li><a href="./agenda_tu_cita.php"><i class="fa fa-angle-double-right"></i>Agenda tu cita</a></li>
+                        <li><a href="./certificado.php"><i class="fa fa-angle-double-right"></i>Certificado</a></li>
+                        <li><a href="./corporativo.php"><i class="fa fa-angle-double-right"></i>Corporativo</a></li>
+                        <li><a href="./trabaja-con-nosotros.php"><i class="fa fa-angle-double-right"></i>Trabaja con nosotros</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4">
+                    <h5>Políticas</h5>
+                    <ul class="list-unstyled quick-links">
+                        <li><a href="./politicas.php"><i class="fa fa-angle-double-right"></i>Políticas</a></li>
+                        <li><a href="./politicasetica.php"><i class="fa fa-angle-double-right"></i>Políticas etica</a></li>
+                        <li><a href="./politicastratamiento.php"><i class="fa fa-angle-double-right"></i>Políticas tratamiento</a></li>
+                        <li><a href="./condiciones.php"><i class="fa fa-angle-double-right"></i>Condiciones para el servicio</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4">
+                    <h5>Contactos</h5>
+                    <ul class="list-unstyled quick-links">
+                        <li><a href="./contactos.php"><i class="fa fa-angle-double-right"></i>Contactos</a></li>
+                        <li>Teléfono: (604) 4480265</li>
+                        <li>Sede Principal Medellín (Antioquia)</li>
+                        <li>CALLE 10 SUR 48 - 62 Barrio Aguacatala 2</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 mt-2">
+                    <ul class="list-unstyled list-inline social text-center">
+                        <li class="list-inline-item"><a href="https://m.facebook.com/people/Eyc-Ingenieria-Sas/100089364634242/"><i class="fa fa-facebook"></i></a></li>
+                        <li class="list-inline-item"><a href="https://www.instagram.com/eycingenieria/?igsh=MXVyYWZ4NHV1ZzJ1dA%3D%3D"><i class="fa fa-instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="mailto:pqr@eyc.com.co" target="_blank"><i class="fa fa-envelope"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="container text-center">
+                <p>&copy; 2024 E&C Ingeniería S.A.S. Todos los derechos reservados.</p>
+            </div>
+        </div>
 
+    </section>
+    <!-- ./Footer -->
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="./js/PF.js"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
 
