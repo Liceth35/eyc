b8fa3969d8c2c838e2aac17ba698267aacb854c4
Liceth35 <liceth2006valderrama@gmail.com>
2024-10-07 19:48:35 -0500

    preguntas frecuentes

--- Preguntas_Frecuentes.php
+++ Preguntas_Frecuentes.php
@@ -1,115 +1,234 @@
 <!DOCTYPE html>
+
 <html lang="es">
+
 <head>
+
     <meta charset="UTF-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Preguntas Frecuentes</title>
+
     <link rel="stylesheet" href="./css/preguntasfrecuentes.css">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
     <link rel="shortcut icon" href="images/New_Logo_EyC2024_vertical-removebg-preview.png">
+
 </head>
+
 <body>
-    <!-- Navbar -->
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top navbar-color">
-            <div class="container">
-                <!-- LOGO -->
-                <a class="" href="index.php"><img class="logo" src="./images/New_Logo_EyC2024-removebg-preview.png" alt=""></a>
-                <!-- Toggler/collapsibe Button -->
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <!-- Navbar links -->
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link" href="index.php"><strong>INICIO</strong></a></li>
-                        <li class="nav-item"><a class="nav-link" href="nosotros.php"><strong>NOSOTROS</strong></a></li>
-                        <li class="nav-item"><a class="nav-link" href="contactos.php"><strong>CONTACTOS</strong></a></li>
-                        <li class="nav-item"><a class="nav-link" href="servicios.php"><strong>SERVICIOS</strong></a></li>
-                        <li class="nav-item"><a class="nav-link" href="agendar_cita.php"><strong>AGENDA TU CITA</strong></a></li>
-                        <li class="nav-item"><a class="nav-link" href="certificado.php"><strong>CERTIFICADO</strong></a></li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="trabaja-con-nosotros.php"><strong>TEAM</strong></a>
-                        </li>
-                        <li class="nav-item"><a class="nav-link" href="./login.php"><strong>CORPORATIVO</strong></a></li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPoliticas" role="button" data-bs-toggle="dropdown" aria-expanded="false">POLÍTICAS</a>
-                            <ul class="dropdown-menu dropdown-team-politicas" aria-labelledby="navbarDropdownPoliticas">
-                                <li><a class="dropdown-item" href="./politicas.php">Políticas</a></li>
-                                <li><a class="dropdown-item" href="./politicasetica.php">Política ética</a></li>
-                                <li><a class="dropdown-item" href="./politicastratamiento.php">Políticas tratamiento</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
+
+<!-- Navbar -->
+
+<header>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top navbar-color">
+
+        <div class="container">
+
+            <!-- LOGO -->
+
+            <a class="navbar-brand" href="index.php">
+
+                <img class="logo" src="./images/New_Logo_EyC2024-removebg-preview.png" alt="Logo">
+
+            </a>
+
+            <!-- Toggler/collapsibe Button -->
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+
+                <span class="navbar-toggler-icon"></span>
+
+            </button>
+
+            <!-- Navbar links -->
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+
+                <ul class="navbar-nav ms-auto">
+
+                    <li class="nav-item"><a class="nav-link" href="index.php"><strong>INICIO</strong></a></li>
+
+                    <li class="nav-item"><a class="nav-link" href="nosotros.php"><strong>NOSOTROS</strong></a></li>
+
+                    <li class="nav-item"><a class="nav-link" href="contactos.php"><strong>CONTACTOS</strong></a></li>
+
+                    <li class="nav-item"><a class="nav-link" href="servicios.php"><strong>SERVICIOS</strong></a></li>
+
+                    <li class="nav-item"><a class="nav-link" href="agendar_cita.php"><strong>AGENDA TU CITA</strong></a></li>
+
+                    <li class="nav-item"><a class="nav-link" href="certificado.php"><strong>CERTIFICADO</strong></a></li>
+
+                    <li class="nav-item"><a class="nav-link" href="trabaja-con-nosotros.php"><strong>TEAM</strong></a></li>
+
+                    <li class="nav-item dropdown">
+
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPoliticas" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+
+                            POLÍTICAS
+
+                        </a>
+
+                        <ul class="dropdown-menu dropdown-team-politicas" aria-labelledby="navbarDropdownPoliticas">
+
+                            <li><a class="dropdown-item" href="./politicas.php">Políticas</a></li>
+
+                            <li><a class="dropdown-item" href="./politicasetica.php">Política ética</a></li>
+
+                            <li><a class="dropdown-item" href="./politicastratamiento.php">Políticas tratamiento</a></li>
+
+                        </ul>
+
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="./login.php"><strong>CORPORATIVO</strong></a></li>
+                </ul>
+
             </div>
-        </nav>
-    </header>
+
+        </div>
+
+    </nav>
+
+</header>
+
+
+
+
 
     <div class="faq-container">
+
         <h1>Preguntas Frecuentes</h1>
+
         <div class="faq-item">
+
             <button class="faq-question">¿Cómo solicito mi revisión de gas?</button>
+
             <div class="faq-answer">
+
                 <p>Para solicitar una revisión de gas, puede agendar una cita a través de nuestra página web en la sección de "Agenda tu cita". También puede contactarnos directamente a través de nuestra línea de atención al cliente.</p>
+
             </div>
+
         </div>
+
         <div class="faq-item">
+
             <button class="faq-question">¿Cada cuánto debo hacer revisar mi instalación de gas?</button>
+
             <div class="faq-answer">
+
                 <p>Las instalaciones de gas deben ser revisadas periódicamente cada cinco años. Sin embargo, recomendamos realizar una inspección si nota cualquier irregularidad en el funcionamiento de sus equipos o detecta algún olor a gas.</p>
+
             </div>
+
         </div>
+
         <div class="faq-item">
+
             <button class="faq-question">En caso de que en la revisión de mi instalación se encuentren problemas de seguridad, ¿Qué debo hacer?</button>
+
             <div class="faq-answer">
+
                 <p>Si durante la revisión se identifican problemas de seguridad, nuestro equipo le proporcionará un informe detallado con las recomendaciones necesarias. Es fundamental que siga estas recomendaciones y realice las reparaciones necesarias lo antes posible para garantizar la seguridad de su hogar o negocio.</p>
+
             </div>
+
         </div>
+
         <div class="faq-item">
+
             <button class="faq-question">¿Cuánto cuesta la revisión periódica de la instalación de gas?</button>
+
             <div class="faq-answer">
+
                 <p>El costo de la revisión periódica puede variar según el tipo y la ubicación de la instalación. Para obtener un presupuesto exacto, le invitamos a contactarnos directamente por PQRS de nuestra página web.</p>
+
             </div>
+
         </div>
+
         <div class="faq-item">
+
             <button class="faq-question">¿Cuánto cuesta la revisión de mi instalación nueva?</button>
+
             <div class="faq-answer">
+
                 <p>El costo de la revisión de una instalación nueva depende de factores como el tamaño y la complejidad de la instalación. Para un presupuesto detallado, le recomendamos solicitar una cotización personalizada a través de nuestra plataforma o en contacto con nuestros asesores.</p>
+
             </div>
+
         </div>
+
         <div class="faq-item">
+
             <button class="faq-question">¿Cuánto cuesta la revisión de mi instalación nueva industrial?</button>
+
             <div class="faq-answer">
+
                 <p>Las revisiones de instalaciones industriales suelen tener un costo superior debido a su mayor complejidad y tamaño. Por favor, comuníquese con nosotros para recibir una cotización específica para su instalación industrial.</p>
+
             </div>
+
         </div>
+
         <div class="faq-item">
+
             <button class="faq-question">¿Dónde hay cobertura de nuestros servicios?</button>
+
             <div class="faq-answer">
+
                 <p>Ofrecemos cobertura en varios municipios y áreas. Para verificar si su ubicación está dentro de nuestra zona de servicio, puede revisar "Agenda tu cita" en nuestro sitio web o contactarnos directamente. Si su municipio no aparece en la lista, le sugerimos contactarnos a través de la sección de PQRS para obtener más información.</p>
+
             </div>
+
         </div>
+
         <div class="faq-item">
+
             <button class="faq-question">¿Qué debo tener listo cuando llegue el inspector a realizar la revisión?</button>
+
             <div class="faq-answer">
+
                 <p>Asegúrese de tener accesible toda la instalación de gas y que los equipos estén en buen estado y limpios. Además, es recomendable tener a mano los documentos de instalación y cualquier certificado previo de revisión.</p>
+
             </div>
+
         </div>
+
         <div class="faq-item">
+
             <button class="faq-question">¿Cuál es el método de pago?</button>
+
             <div class="faq-answer">
+
                 <p>Aceptamos diferentes métodos de pago, incluyendo transferencias bancarias, pagos con tarjeta de crédito o débito, y pagos en efectivo en nuestras oficinas. También puede realizar el pago en línea a través de nuestra página web.</p>
+
             </div>
+
         </div>
+
         <div class="faq-item">
+
             <button class="faq-question">¿Cuáles son las condiciones de prestación del servicio?</button>
+
             <div class="faq-answer">
+
                 <p>Las condiciones de prestación del servicio incluyen la obligatoriedad de cumplir con las normativas de seguridad vigentes, la posibilidad de realizar inspecciones sin previo aviso, y la responsabilidad del cliente de realizar las reparaciones necesarias en caso de detectar problemas. Puede consultar las condiciones completas en nuestra página web o solicitarlas a través de nuestro servicio de atención al cliente.</p>
+
             </div>
+
         </div>
+
     </div>
+
     <script src="./js/PF.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 </body>
+
 </html>
+
